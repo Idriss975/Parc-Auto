f53f6e91ccc6a6fe3831ce7a1c49635a71d90ef7
ElAchker <ADMIN@DESKTOP-0HDF3CT>
2022-07-12 10:30:29 +0100

    create size Form transport et Reparation

--- ParcAuto/Forms/Transport.Designer.cs
+++ ParcAuto/Forms/Transport.Designer.cs
@@ -29,10 +29,17 @@ namespace ParcAuto.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Transport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1111, 600);
+            this.Name = "Transport";
             this.Text = "Transport";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
