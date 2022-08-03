d1d7e457d75117a8b821ece58f3224b27bd9eb85
ElAchker <ADMIN@DESKTOP-0HDF3CT>
2022-08-03 16:43:26 +0100

    Scroll dgv carburant

--- ParcAuto/Forms/Carburants.cs
+++ ParcAuto/Forms/Carburants.cs
@@ -127,25 +127,28 @@ namespace ParcAuto.Forms
         {
             try
             {
-                GLB.id_Carburant = Convert.ToInt32(dgvCarburant.CurrentRow.Cells[13].Value);
-                string Entite = dgvCarburant.CurrentRow.Cells[0].Value.ToString();
-                string Benificiaire = dgvCarburant.CurrentRow.Cells[1].Value.ToString();
-                string vehicules = dgvCarburant.CurrentRow.Cells[2].Value.ToString();
-                string Marque = dgvCarburant.CurrentRow.Cells[3].Value.ToString();
-                DateTime DateOper = DateTime.ParseExact(dgvCarburant.CurrentRow.Cells[4].Value.ToString(),"d/M/yyyy",System.Globalization.CultureInfo.InvariantCulture); 
-                string lieu = dgvCarburant.CurrentRow.Cells[5].Value.ToString();
-                string KM = dgvCarburant.CurrentRow.Cells[6].Value.ToString();
-                string pourcentage = dgvCarburant.CurrentRow.Cells[7].Value.ToString();
-                string omn = dgvCarburant.CurrentRow.Cells[8].Value.ToString().Substring(0, dgvCarburant.CurrentRow.Cells[8].Value.ToString().Length - 3);
-                string Dfix = dgvCarburant.CurrentRow.Cells[9].Value.ToString();
-                string DMiss = dgvCarburant.CurrentRow.Cells[10].Value.ToString();
-                string Dhebdo = dgvCarburant.CurrentRow.Cells[11].Value.ToString();
-                string Dexceptionnel = dgvCarburant.CurrentRow.Cells[12].Value.ToString();
-                string Observation = dgvCarburant.CurrentRow.Cells[14].Value.ToString(); 
+                int pos = dgvCarburant.CurrentRow.Index;
+                GLB.id_Carburant = Convert.ToInt32(dgvCarburant.Rows[pos].Cells[13].Value);
+                string Entite = dgvCarburant.Rows[pos].Cells[0].Value.ToString();
+                string Benificiaire = dgvCarburant.Rows[pos].Cells[1].Value.ToString();
+                string vehicules = dgvCarburant.Rows[pos].Cells[2].Value.ToString();
+                string Marque = dgvCarburant.Rows[pos].Cells[3].Value.ToString();
+                DateTime DateOper = DateTime.ParseExact(dgvCarburant.Rows[pos].Cells[4].Value.ToString(),"d/M/yyyy",System.Globalization.CultureInfo.InvariantCulture); 
+                string lieu = dgvCarburant.Rows[pos].Cells[5].Value.ToString();
+                string KM = dgvCarburant.Rows[pos].Cells[6].Value.ToString();
+                string pourcentage = dgvCarburant.Rows[pos].Cells[7].Value.ToString();
+                string omn = dgvCarburant.Rows[pos].Cells[8].Value.ToString().Substring(0, dgvCarburant.Rows[pos].Cells[8].Value.ToString().Length - 3);
+                string Dfix = dgvCarburant.Rows[pos].Cells[9].Value.ToString();
+                string DMiss = dgvCarburant.Rows[pos].Cells[10].Value.ToString();
+                string Dhebdo = dgvCarburant.Rows[pos].Cells[11].Value.ToString();
+                string Dexceptionnel = dgvCarburant.Rows[pos].Cells[12].Value.ToString();
+                string Observation = dgvCarburant.Rows[pos].Cells[14].Value.ToString(); 
                 MajCarburants maj = new MajCarburants(Entite, Benificiaire, vehicules, Marque, DateOper, lieu,KM,pourcentage, omn, Dfix, DMiss, Dhebdo, Dexceptionnel, Observation);
                 Commandes.Command = Choix.modifier;
                 maj.ShowDialog();
                 RemplirLaGrille();
+                dgvCarburant.Rows[pos].Selected = true;
+                dgvCarburant.FirstDisplayedScrollingRowIndex = pos;
             }
             catch (ArgumentOutOfRangeException)
             {
