0af8cd2c21030c4d3f385aeb6c8203b3f5644bca
ElAchker <ADMIN@DESKTOP-0HDF3CT>
2022-08-08 15:20:45 +0100

    Les Consommation de toutes l'annee Carte Free

--- ParcAuto/Forms/EtatRecap.cs
+++ ParcAuto/Forms/EtatRecap.cs
@@ -20,6 +20,7 @@ namespace ParcAuto.Forms
 
         private void EtatRecap_Load(object sender, EventArgs e)
         {
+            //Les Consommation des Cartes Free
             GLB.Cmd.CommandText = $"select Report,Achat from EtatRecapCartefree where Annee = {DateTime.Now.Year}";
             GLB.Con.Open();
             GLB.dr = GLB.Cmd.ExecuteReader();
@@ -30,7 +31,7 @@ namespace ParcAuto.Forms
                 SumStockCarteFree.Text = String.Format("{0:0.00}", (float.Parse( GLB.dr[0].ToString()) + float.Parse( GLB.dr[1].ToString())));
             }
             GLB.dr.Close();
-            GLB.Cmd.CommandText = $"select Fixe , Autre from CarteFree where dateCarte >= '{DateTime.Now.Year}-01-01' and dateCarte < '{DateTime.Now.Year}-04-01'";
+            GLB.Cmd.CommandText = $"select Fixe , Autre from CarteFree where dateCarte >= '{DateTime.Now.Year}-01-01' and dateCarte <= '{DateTime.Now.Year}-03-31'";
             GLB.dr = GLB.Cmd.ExecuteReader();
 
             while (GLB.dr.Read())
@@ -38,9 +39,42 @@ namespace ParcAuto.Forms
                 trim1CarteFree.Text = String.Format("{0:0.00}", (double.Parse(trim1CarteFree.Text) + (GLB.dr.IsDBNull(0) ? 0.0:(double)GLB.dr[0]) + (GLB.dr.IsDBNull(1)?0.0:(double)GLB.dr[1])));
             }
             GLB.dr.Close();
-            GLB.Con.Close();
+
+            GLB.Cmd.CommandText = $"select Fixe , Autre from CarteFree where dateCarte >= '{DateTime.Now.Year}-04-01' and dateCarte < '{DateTime.Now.Year}-06-30'";
+            GLB.dr = GLB.Cmd.ExecuteReader();
+
+            while (GLB.dr.Read())
+            {
+                trim2CarteFree.Text = String.Format("{0:0.00}", (double.Parse(trim2CarteFree.Text) + (GLB.dr.IsDBNull(0) ? 0.0 : (double)GLB.dr[0]) + (GLB.dr.IsDBNull(1) ? 0.0 : (double)GLB.dr[1])));
+            }
+            GLB.dr.Close();
+
+            GLB.Cmd.CommandText = $"select Fixe , Autre from CarteFree where dateCarte >= '{DateTime.Now.Year}-07-01' and dateCarte < '{DateTime.Now.Year}-09-30'";
+            GLB.dr = GLB.Cmd.ExecuteReader();
+
+            while (GLB.dr.Read())
+            {
+                trim3CarteFree.Text = String.Format("{0:0.00}", (double.Parse(trim3CarteFree.Text) + (GLB.dr.IsDBNull(0) ? 0.0 : (double)GLB.dr[0]) + (GLB.dr.IsDBNull(1) ? 0.0 : (double)GLB.dr[1])));
+            }
+            GLB.dr.Close();
+
+            GLB.Cmd.CommandText = $"select Fixe , Autre from CarteFree where dateCarte >= '{DateTime.Now.Year}-10-01' and dateCarte < '{DateTime.Now.Year}-12-31'";
+            GLB.dr = GLB.Cmd.ExecuteReader();
+
+            while (GLB.dr.Read())
+            {
+                trim4CarteFree.Text = String.Format("{0:0.00}", (double.Parse(trim4CarteFree.Text) + (GLB.dr.IsDBNull(0) ? 0.0 : (double)GLB.dr[0]) + (GLB.dr.IsDBNull(1) ? 0.0 : (double)GLB.dr[1])));
+            }
+            GLB.dr.Close();
+           
             sumtrimestres.Text = String.Format("{0:0.00}", (double.Parse(trim1CarteFree.Text) + double.Parse(trim2CarteFree.Text) + double.Parse(trim3CarteFree.Text) + double.Parse(trim4CarteFree.Text)));
             Disponible.Text = String.Format("{0:0.00}", (double.Parse(SumStockCarteFree.Text) - double.Parse(sumtrimestres.Text)));
+
+            GLB.Cmd.CommandText = $"update EtatRecapCartefree set TotalReport_Achat = {double.Parse( SumStockCarteFree.Text)} , trim1 = {double.Parse(trim1CarteFree.Text)}, trim2 = {double.Parse(trim2CarteFree.Text)}, trim3 = {double.Parse(trim3CarteFree.Text)}, trim4 = {double.Parse(trim4CarteFree.Text)}, Totalconsommation = {double.Parse(sumtrimestres.Text)}, DispoAnneeProch ={double.Parse(Disponible.Text)} where Annee ={DateTime.Now.Year}";
+            GLB.Cmd.ExecuteNonQuery();
+            GLB.Con.Close();
+            //Les Consommation des Cartes Free
+
         }
     }
 }
