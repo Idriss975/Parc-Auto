a01c43298087f5fbb1062c30c8c9e229f66067ac
ElAchker <ADMIN@DESKTOP-0HDF3CT>
2022-08-26 10:57:27 +0100

    Afficher les buttons selon l'utilisateur connecté

--- ParcAuto/Form1.cs
+++ ParcAuto/Form1.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ParcAuto.Classes_Globale;
 using ParcAuto.Forms;
 
 namespace ParcAuto
@@ -78,11 +79,74 @@ namespace ParcAuto
             openChildForm(new Forms.Transport(),sender);
             //hideSubMenu();
         }
-
+        private void Permissions()
+        {
+            GLB.Cmd.CommandText = "SELECT  pri.name As Username " +
+                   ",       pri.type_desc AS[User Type] " +
+                   ", permit.permission_name AS[Permission] " +
+                   ", permit.state_desc AS[Permission State] " +
+                   ", permit.class_desc Class " +
+                   ", object_name(permit.major_id) AS[Object Name] " +
+                   "FROM sys.database_principals pri " +
+                   "LEFT JOIN " +
+                   "sys.database_permissions permit " +
+                   "ON permit.grantee_principal_id = pri.principal_id " +
+                   $"where pri.name = SUSER_NAME()";
+            GLB.Con.Open();
+            GLB.dr = GLB.Cmd.ExecuteReader();
+            while (GLB.dr.Read())
+            {
+                if ((GLB.dr[5].ToString() == "CarburantSNTLPRD" || GLB.dr[5].ToString() == "CarburantVignettes" || GLB.dr[5].ToString() == "CarteFree") && GLB.dr[2].ToString() =="SELECT")
+                {
+                    if (GLB.dr[3].ToString() == "DENY")
+                    {
+                        btnVignettes.Visible = false;
+                    }
+                }
+                else if((GLB.dr[5].ToString() == "Reparation" || GLB.dr[5].ToString() == "ReparationPRDSNTL") && GLB.dr[2].ToString() == "SELECT")
+                {
+                    if (GLB.dr[3].ToString() == "DENY")
+                    {
+                        btnReparation.Visible = false;
+                    }
+                }
+                else if (GLB.dr[5].ToString() == "Transport" && GLB.dr[2].ToString() == "SELECT")
+                {
+                    if (GLB.dr[3].ToString() == "DENY")
+                    {
+                        btnTransport.Visible = false;
+                    }
+                }
+                else if ((GLB.dr[5].ToString() == "Vehicules" || GLB.dr[5].ToString() == "VehiculesPRD") && GLB.dr[2].ToString() == "SELECT")
+                {
+                    if (GLB.dr[3].ToString() == "DENY")
+                    {
+                        btnParcAuto.Visible = false;
+                    }
+                }
+                else if (GLB.dr[5].ToString() == "Conducteurs" && GLB.dr[2].ToString() == "SELECT")
+                {
+                    if (GLB.dr[3].ToString() == "DENY")
+                    {
+                        btnConducteurs.Visible = false;
+                    }
+                }
+                else if (GLB.dr[5].ToString() == "EtatJournalier" && GLB.dr[2].ToString() == "SELECT")
+                {
+                    if (GLB.dr[3].ToString() == "DENY")
+                    {
+                        btnEtatJournalier.Visible = false;
+                    }
+                }
+            }
+            GLB.dr.Close();
+            GLB.Con.Close();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Maximized;
             customizeDesign();
+            Permissions();
         }
 
         private void btnVehicules_Click(object sender, EventArgs e)
