92f0347a0c427dced51e26d80183074d5108cb4e
ELachker02 <amineelachker02@gmail.com>
2022-12-27 12:00:08 +0100

    ljds

--- ParcAuto/Forms/Login.cs
+++ ParcAuto/Forms/Login.cs
@@ -53,9 +53,9 @@ namespace ParcAuto.Forms
         {
             //GLB.Con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Parc_Automobile;Integrated Security=True");
             //GLB.Con = new SqlConnection($"Data Source=DAL1251\\SQLEXPRESS,1433;Network Library=DBMSSOCN;Initial Catalog=Parc_Automobile;Persist Security Info=True;User ID={txtuser.Text.Trim()};Password={txtpass.Text.Trim()}");
-            GLB.Con = new SqlConnection($"Data Source=DESKTOP-0HDF3CT\\SQLEXPRESS,1434;Network Library=DBMSSOCN;Initial Catalog=Parc_Automobile;Persist Security Info=True;User ID={txtuser.Text.Trim()};Password={txtpass.Text.Trim()}");
+            GLB.Con = new SqlConnection($"Data Source=DESKTOP-PS2HKMR\\SQLEXPRESS,1433;Network Library=DBMSSOCN;Initial Catalog=Parc_Automobile;User ID={txtuser.Text.Trim()};Password={txtpass.Text.Trim()}");
             GLB.Cmd = GLB.Con.CreateCommand();
-            GLB.Cmd.CommandTimeout = 8;
+            GLB.Cmd.CommandTimeout = 5;
             try
             {
                 GLB.Con.Open();
